909754f3f825d23f616b5a959328303ce10076c7
Erich Welz <erichwelz@gmail.com>
2020-11-22 10:53:27 -0500

    feat: generic method for gathering employees

--- src/aurora/scheduler.js
+++ src/aurora/scheduler.js
@@ -1,13 +1,5 @@
 const { BUILDING_TYPES, EMPLOYEE_TYPES, Job } = require('./models')
 
-// buildings is an array of buildings...
-// employees is an array of employees
-
-// schedule is an array of Jobs
-
-// const buildingRequirements: {
-//   EMPLOYEE_TYPES.cEmployee =
-// }
 const SSHOME = [
   { employeeType: EMPLOYEE_TYPES.CINSTALLER, number: 1 }
 ]
@@ -17,13 +9,46 @@ const TSHOME = [
   { employeeType: [EMPLOYEE_TYPES.PINSTALLER, EMPLOYEE_TYPES.HANDYMAN], number: 1 }
 ]
 const ALL_EMPLOYEE_TYPES = Object.values(EMPLOYEE_TYPES)
-
 const COMMERCIAL = [
   { employeeType: EMPLOYEE_TYPES.CINSTALLER, number: 2 },
   { employeeType: EMPLOYEE_TYPES.PINSTALLER, number: 2 },
   { employeeType: ALL_EMPLOYEE_TYPES, number: 4 }
 ]
 
+// buildingType one of constants SSHOME/TSHOME/COMMERCIAL
+const gatherEmployees = (buildingType, availableEmployees) => {
+  const jobEmployeeIds = []
+
+  buildingType.forEach(({ employeeType, number }) => {
+    const employeeTypeIds = availableEmployees
+      .filter(e => employeeType.includes(e.employeeType) &&
+          !jobEmployeeIds.includes(e.id))
+      .map(x => { return x.id })
+      .slice(0, number)
+
+    jobEmployeeIds.push(...employeeTypeIds)
+  })
+
+  const numberOfEmployeesRequired = buildingType
+    .map(x => { return x.number })
+    .reduce((total, amount) => total + amount)
+
+  if (jobEmployeeIds.length === numberOfEmployeesRequired) {
+    // marks resources unvailable if enough people available
+    availableEmployees.forEach(e => {
+      if (jobEmployeeIds.includes(e.id)) {
+        e.available = false
+      }
+    })
+    return jobEmployeeIds
+  }
+  return false
+}
+
+// buildings is an array of buildings...
+// employees is an array of employees
+
+// schedule is an array of Jobs
 const schedule = (buildings, employees) => {
   const schedule = []
 
@@ -31,87 +56,21 @@ const schedule = (buildings, employees) => {
     const availableEmployees = employees.filter(e => e.available === true)
 
     if (building.buildingType === BUILDING_TYPES.SSHOME) {
-      const jobEmployeeIds = []
+      const jobEmployeeIds = gatherEmployees(SSHOME, availableEmployees)
 
-      SSHOME.forEach(({ employeeType, number }) => {
-        const employeeTypeIds = availableEmployees
-          .filter(e => employeeType.includes(e.employeeType) &&
-          !jobEmployeeIds.includes(e.id))
-          .map(x => { return x.id })
-          .slice(0, number)
-
-        jobEmployeeIds.push(...employeeTypeIds)
-      })
-
-      const numberOfEmployeesRequired = SSHOME
-        .map(x => { return x.number })
-        .reduce((total, amount) => total + amount)
-
-      if (jobEmployeeIds.length === numberOfEmployeesRequired) {
-        // marks resources unvailable if enough people available
-        availableEmployees.forEach(e => {
-          if (jobEmployeeIds.includes(e.id)) {
-            e.available = false
-          }
-        })
-        schedule.push(Job({ buildingId: building.id, employeeIds: jobEmployeeIds }))
-      }
+      if (jobEmployeeIds) { schedule.push(Job({ buildingId: building.id, employeeIds: jobEmployeeIds })) }
     }
 
     if (building.buildingType === BUILDING_TYPES.TSHOME) {
-      const jobEmployeeIds = []
+      const jobEmployeeIds = gatherEmployees(TSHOME, availableEmployees)
 
-      TSHOME.forEach(({ employeeType, number }) => {
-        const employeeTypeIds = availableEmployees
-          .filter(e => employeeType.includes(e.employeeType) &&
-          !jobEmployeeIds.includes(e.id))
-          .map(x => { return x.id })
-          .slice(0, number)
-
-        jobEmployeeIds.push(...employeeTypeIds)
-      })
-
-      const numberOfEmployeesRequired = TSHOME
-        .map(x => { return x.number })
-        .reduce((total, amount) => total + amount)
-
-      if (jobEmployeeIds.length === numberOfEmployeesRequired) {
-        // marks resources unvailable if enough people available
-        availableEmployees.forEach(e => {
-          if (jobEmployeeIds.includes(e.id)) {
-            e.available = false
-          }
-        })
-        schedule.push(Job({ buildingId: building.id, employeeIds: jobEmployeeIds }))
-      }
+      if (jobEmployeeIds) { schedule.push(Job({ buildingId: building.id, employeeIds: jobEmployeeIds })) }
     }
 
     if (building.buildingType === BUILDING_TYPES.COMMERCIAL) {
-      const jobEmployeeIds = []
+      const jobEmployeeIds = gatherEmployees(COMMERCIAL, availableEmployees)
 
-      COMMERCIAL.forEach(({ employeeType, number }) => {
-        const employeeTypeIds = availableEmployees
-          .filter(e => employeeType.includes(e.employeeType) &&
-          !jobEmployeeIds.includes(e.id))
-          .map(x => { return x.id })
-          .slice(0, number)
-
-        jobEmployeeIds.push(...employeeTypeIds)
-      })
-
-      const numberOfEmployeesRequired = COMMERCIAL
-        .map(x => { return x.number })
-        .reduce((total, amount) => total + amount)
-
-      if (jobEmployeeIds.length === numberOfEmployeesRequired) {
-        // marks resources unvailable if enough people available
-        availableEmployees.forEach(e => {
-          if (jobEmployeeIds.includes(e.id)) {
-            e.available = false
-          }
-        })
-        schedule.push(Job({ buildingId: building.id, employeeIds: jobEmployeeIds }))
-      }
+      if (jobEmployeeIds) { schedule.push(Job({ buildingId: building.id, employeeIds: jobEmployeeIds })) }
     }
   })
 
